e7a11d6889e662108ffb0a902bc7e6a52ffbb574
FforFrodo <68970147+FforFrodo@users.noreply.github.com>
2020-12-17 15:00:36 +0000

    Add files via upload
    /helloworld returns 'Hello Stranger'
    /helloworld?name=AlfredENeumann returns 'Alfred E Neumann'
    /versionz returns a JSON with Githash and name of the project

--- Endocode.py
+++ Endocode.py
@@ -1,49 +1,31 @@
-#REST API with Flask for Endocode Technical challenge level 1
+#REST API with Flask for Endocode technical challenge Level 1
 #Kevin Donnelly
 
 from flask import Flask, jsonify, request
-import subprocess
+import subprocess, re
 app = Flask(__name__)
 
 
-# /helloworld retunrs 'Hello Stranger' 
+# /helloworld returns 'Hello Stranger' 
 # http://0.0.0.0:8080/helloworld
 
-#/helloworld?name + <StringString> returns 'Hello <String String>'
+#/helloworld?name=AlfredENeumann (any filtered value) returns 'Hello Alfred E Neumann'
 # http://0.0.0.0:8080/helloworld?name=
-@app.route('/helloworld', methods=['GET', 'POST'])
-def helloworld():
-
-    if (request.method == 'GET'):
-        name = request.args.get('name', default = None, type = str)
-
-        for i in range(len(name)-1)[::-1]:
-            if name[i].isupper() and name[i+1].islower():
-                name = name[:i]+' '+name[i:] 
-            if name[i].isupper() and name[i-1].islower():
-                name = name[:i]+' '+name[i:]
-        b =  name.split() #Camel case string is split into a list 
-        a = ' ' #blank spaces for .join
-        NewName = a.join(b) #joins strings from list into 1 string with spaces
-        return 'Hello ' + (NewName) #Success
-
+@app.route('/helloworld', methods=['GET'])
+def hello_world():
+    if request.args:
+        query_name = request.args.get("name", "")
+        user_name = (re.sub(r"(?<=\w)([A-Z])", r" \1", query_name)).title()
+        return 'Hello ' + (user_name)
     else:
-        return 'Hello Stranger' #Doesn't work
-    
-    #---This is from a tutorial
-    #if (request.method == 'POST'):
-        #some_json = request.get_json()
-        #return jsonify({'You sent': some_json}), 201
-    #else:
-        #return jsonify({"about":"Hello Stranger"})
-
+        return 'Hello Stranger'
 
 # Returns a JSON with Githash and name of the project
 # http://0.0.0.0:8080/versionz
 @app.route('/versionz', methods=['GET'])
 def get_git_revision_hash():
     GitHash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-    return jsonify({'Endocode':GitHash})
+    return jsonify({'Endocode.py':GitHash})
 
 
 #Bonus: multiplication function for testing purposes
@@ -53,12 +35,12 @@ def get_multiply10(num):
     return jsonify({'result':num*10})
 
 
-#Runs on port http://0.0.0.0:8080/
+#Run on port http://0.0.0.0:8080/
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=8080, debug=True)
 
-
+#Test URLs
 # http://0.0.0.0:8080/helloworld
-# http://0.0.0.0:8080/helloworld?name=  <...>
+# http://0.0.0.0:8080/helloworld?name=AlfredENeumann
 # http://0.0.0.0:8080/versionz
 # http://0.0.0.0:8080/multi/
